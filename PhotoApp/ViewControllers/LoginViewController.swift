0a0d16ae95e51c4737fede4b472a4dbb61764fd0
Michael Shustov <shustovmm@gmail.com>
2021-10-30 17:20:51 +0300

    - implement Firebase email auth for login view controller

--- PhotoApp/ViewControllers/LoginViewController.swift
+++ PhotoApp/ViewControllers/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseEmailAuthUI
 
 class LoginViewController: UIViewController {
 
@@ -16,6 +17,50 @@ class LoginViewController: UIViewController {
     }
 
     @IBAction func loginTapped(_ sender: Any) {
+        
+        // Create Firebase auth ui object
+        let authUI = FUIAuth.defaultAuthUI()
+        
+        // Check that it isn't nil
+        if let authUI = authUI {
+            
+            // Set self as delegate for the authUI
+            authUI.delegate = self
+            
+            // Set sign in providers
+            authUI.providers = [FUIEmailAuth()]
+            
+            // Get the prebuilt UI view controller
+            let authViewController = authUI.authViewController()
+            
+            // Present it
+            present(authViewController, animated: true, completion: nil)
+        }
     }
 
+}
+
+extension LoginViewController: FUIAuthDelegate {
+    
+    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
+        
+        if error != nil {
+            // Display error message and return
+            return
+        }
+        
+        let user = authDataResult?.user
+        
+        if let user = user {
+            
+            // Got a user
+            // Check on the database side if user has a profile
+            
+            // If not, go to create profile view controller
+            
+            // If so, to tab bar controller
+            
+        }
+    }
+    
 }
