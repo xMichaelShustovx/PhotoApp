4e0601a4d0618961ce4d5777a773a4227f559872
Michael Shustov <shustovmm@gmail.com>
2021-11-05 17:20:55 +0300

    - add table view delegate methods for feed view controller - add image cache service class to store the images - implement pull-to-refresh gesture

--- PhotoApp/ViewControllers/FeedViewController.swift
+++ PhotoApp/ViewControllers/FeedViewController.swift
@@ -15,6 +15,13 @@ class FeedViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Set view controller as the table view data source and delegate
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        // Add pull to refresh
+        addRefreshControl()
 
         // Call the PhotoService to retrive the photos
         PhotoService.retrievePhotos { photos in
@@ -27,5 +34,59 @@ class FeedViewController: UIViewController {
         }
         
     }
+    
+    func addRefreshControl() {
+     
+        // Add refresh control
+        let refresh = UIRefreshControl()
+        
+        // Set target
+        refresh.addTarget(self, action: #selector(refreshFeed(refreshControl:)), for: .valueChanged)
+        
+        // Add to table view
+        self.tableView.addSubview(refresh)
+    }
+    
+    @objc func refreshFeed(refreshControl: UIRefreshControl) {
+        
+        // Call the photo service
+        PhotoService.retrievePhotos { newPhotos in
+            
+            // Assign new photos to our property
+            self.photos = newPhotos
+            
+            DispatchQueue.main.async {
+                
+                // Refesh table view
+                self.tableView.reloadData()
+                
+                // Stop the spinner
+                refreshControl.endRefreshing()
+            }
+        }
+    }
+}
 
+// MARK: - Table View Delegate Methods
+extension FeedViewController: UITableViewDataSource & UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // Get a Photo Cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.Storyboard.photoCellId, for: indexPath) as? PhotoCell
+        
+        // Get the photo this cell is displaying
+        let currentPhoto = photos[indexPath.row]
+        
+        // Call display photo method of the cell
+        cell?.displayPhoto(photo: currentPhoto)
+        
+        // Return the cell
+        return cell!
+    }
 }
--- PhotoApp/Views/PhotoCell.swift
+++ PhotoApp/Views/PhotoCell.swift
@@ -31,13 +31,72 @@ class PhotoCell: UITableViewCell {
     
     func displayPhoto(photo: Photo) {
         
+        // Reset the image
+        self.photoImageView.image = nil
+        
+        // Set photo property
+        self.photo = photo
+        
         // Set the username
         usernameLabel.text = photo.byUsername
         
         // Set the date
         dateLabel.text = photo.date
         
-        // Download the image
+        // Check that there is a valid download url
+        if photo.url == nil {
+            return
+        }
         
+        // Check if the image is in our image cache
+        if let image = ImageCacheService.getImage(url: photo.url!) {
+            
+            // Use the cached image
+            DispatchQueue.main.async {
+                
+                self.photoImageView.image = image
+            }
+            
+            // Skip the rest of the code as we don't need to download image
+            return
+        }
+        
+        // Download the image
+        let url = URL(string: photo.url!)
+        
+        // Check url isn't nil
+        if url == nil {
+            return
+        }
+        
+        // Use URL session to download image asynchronously
+        let session = URLSession.shared
+        
+        let dataTask = session.dataTask(with: url!) { data, response, error in
+            
+            if error == nil && data != nil {
+                
+                // Check that the image data we downloaded matches the photo this cell is currently supposed to display
+                if url!.absoluteString != self.photo?.url {
+                    
+                    // The url we downloaded doesn't match the url this cell is currently displaying
+                    return
+                }
+                
+                // Get the image view
+                let image = UIImage(data: data!)
+                
+                // Store the image data in cache
+                ImageCacheService.saveImage(url: url!.absoluteString, image: image)
+                
+                // Set the image view
+                DispatchQueue.main.async {
+                    self.photoImageView.image = image
+                    
+                }
+            }
+        }
+        // Start the data task
+        dataTask.resume()
     }
 }
